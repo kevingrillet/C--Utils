367668baaeffd931bbed304e84bd7c1d35674aae
kevingrillet <kevin.grillet@gmail.com>
2024-02-02 12:04:54 +0100

    WIP DocxComparer

---
+++ CSharp Utils/CSharp Utils.Tests/Helpers/DocxComparerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using CSharp_Utils.Helpers;
+
+namespace CSharp_Utils.Tests.Helpers
+{
+    public class DocxComparerTests : DocxComparer
+    {
+        private DocxComparer _docxComparer;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _docxComparer = new DocxComparer();
+        }
+
+        [TestCase("document1", "document2", true)]
+        [TestCase("document1", "document3", false)]
+        [TestCase("document", "document2", false)]
+        [TestCase("", "document2", false)]
+        [TestCase(null, "document2", false)]
+        [TestCase("document1", "document", false)]
+        [TestCase("document1", "", false)]
+        [TestCase("document1", null, false)]
+        public void TestCompare(string document1, string document2, bool expected)
+        {
+            Assert.That(_docxComparer.DocumentsAreEqual($@"Ressources/{document1}.docx", $@"Ressources/{document2}.docx"), Is.EqualTo(expected));
+        }
+    }
+}
