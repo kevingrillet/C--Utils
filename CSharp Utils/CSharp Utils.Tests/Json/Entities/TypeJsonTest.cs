fabd91b8b2dc44f30db0466ad20af810500fb29c
kevingrillet <kevin.grillet@gmail.com>
2024-11-28 12:54:43 +0100

    Update TypeJsonTest.cs

--- CSharp Utils/CSharp Utils.Tests/Json/Entities/TypeJsonTest.cs
+++ CSharp Utils/CSharp Utils.Tests/Json/Entities/TypeJsonTest.cs
@@ -1,6 +1,25 @@
-﻿namespace CSharp_Utils.Tests.Json.Entities;
+﻿using Newtonsoft.Json.Linq;
+using System;
 
-internal class TypeJsonTest
+namespace CSharp_Utils.Tests.Json.Entities;
+
+internal class TypeJsonTest : IEquatable<TypeJsonTest>
 {
     public string Key { get; set; }
+
+    public bool Equals(TypeJsonTest other)
+    {
+        if (other == null) return false;
+        return Key == other.Key;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as TypeJsonTest);
+    }
+
+    public override int GetHashCode()
+    {
+        return Key?.GetHashCode() ?? 0;
+    }
 }
